78fea8e70b68e2257145c0ef18158c5ec41dafd1
ianoliveirasilva <ianoliveira.tec@gmail.com>
2024-03-04 17:32:23 -0300

    Adding top artist

--- views/data.py
+++ views/data.py
@@ -10,10 +10,8 @@ class DataFetcher:
         self.api_key = os.getenv("API_KEY")
         self.api_url = os.getenv("API_URL")
 
-
-
     def get_json_data(self, method):
-        url = f"{self.api_url}?method={method}&user={self.user}&api_key={self.api_key}&period={self.period}&format=json"
+        url = f"{self.api_url}?method={method}&user={self.user}&api_key={self.api_key}&period={self.period}&limit=5&format=json"
         response = requests.get(url)
         return response.json()
 
@@ -23,13 +21,29 @@ class DataFetcher:
         return response.json()
     
     def get_recent_track(self):
-        url = f"{self.api_url}?method=user.getrecenttracks&user={self.user}&api_key={self.api_key}&format=json"
+        url = f"{self.api_url}?method=user.getrecenttracks&user={self.user}&api_key={self.api_key}&limit=5&format=json"
         response = requests.get(url)
         return response.json()
 
     def get_similar_artist(self, artist):
-        url = f"{self.api_url}?method=artist.getinfo&artist={artist}&api_key={self.api_key}&format=json"
+        url = f"{self.api_url}?method=artist.getsimilar&artist={artist}&api_key={self.api_key}&limit=5&format=json"
         response = requests.get(url)
         return response.json()
+    
+    def get_similar_track(self, artist, track):
+        url = f"{self.api_url}?method=track.getsimilar&artist={artist}&track={track}&api_key={self.api_key}&limit=5&format=json"
+        response = requests.get(url)
+        return response.json()
+    
+    def get_track_info(self, artist, track):
+        url = f"{self.api_url}?method=track.getInfo&api_key={self.api_key}&artist={artist}&track={track}&format=json"
+        response = requests.get(url)
+        return response.json()
+    
+    def get_artist_info(self, artist): 
+        url = f"{self.api_url}?method=artist.getinfo&artist={artist}&api_key={self.api_key}&limit=5&format=json"
+        response = requests.get(url)
+        return response.json()
+    
     
     
--- views/main.py
+++ views/main.py
@@ -1,55 +1,29 @@
 from flask import Flask, render_template, request
 
 from data import DataFetcher
+from plot import Plotter
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
-def get_artist_info(fetcher, artist, artist_similar):
+def get_artist_info(fetcher, artist, artist_similar, playcount_artist):
     first_genre = artist_similar['artist']['tags']['tag'][0]['name']
     similar_artists = [similar_artist['name'] for similar_artist in artist_similar['artist']['similar']['artist']]
     return {
         "top_artist": artist,
         "genre": first_genre,
-        "similar_artists": similar_artists
+        "similar_artists": similar_artists,
+        "playcount_artist": playcount_artist
     }
 
 def get_top_artists_and_similars(fetcher, data_artists):
     result = []
     for artist in data_artists['topartists']['artist'][:5]:
+        playcount_artist = artist['playcount']
         top_artist = artist['name']
-        artist_similar = fetcher.get_similar_artist(top_artist)
-        result.append(get_artist_info(fetcher, top_artist, artist_similar))
+        artist_similar = fetcher.get_artist_info(top_artist)
+        result.append(get_artist_info(fetcher, top_artist, artist_similar, playcount_artist))
+    
     return result
-
-def get_top_artists_and_genres(fetcher, data_artists):
-    result = []
-    for artist in data_artists['topartists']['artist']:
-        top_artist = artist['name']
-        artist_similar = fetcher.get_similar_artist(top_artist)
-        if 'artist' in artist_similar and 'tags' in artist_similar['artist'] and 'tag' in artist_similar['artist']['tags'] and len(artist_similar['artist']['tags']['tag']) > 0:
-            result.append(get_artist_info(fetcher, top_artist, artist_similar))
-    return result
-
-def get_recent_artists_and_similars(fetcher, data_recent_track):
-    result = []
-    processed_artists = set()
-    for artist in data_recent_track['recenttracks']['track'][:5]:
-        top_artist = artist['artist']['#text']
-        if top_artist not in processed_artists:
-            processed_artists.add(top_artist)
-            artist_similar = fetcher.get_similar_artist(top_artist)
-            result.append(get_artist_info(fetcher, top_artist, artist_similar))
-    return result
-
-def count_genre_frequency(results):
-    genre_count = {}
-    for item in results:
-        genre = item['genre']
-        genre_count[genre] = genre_count.get(genre, 0) + 1
-
-    genre_count = dict(sorted(genre_count.items(), key=lambda item: item[1], reverse=True)[:7])
-
-    return genre_count
 
 def get_user_input():
     user = request.form.get('user')
@@ -58,30 +32,47 @@ def get_user_input():
 
 def get_data(fetcher):
     data_artists = fetcher.get_json_data("user.gettopartists")
+    data_tracks = fetcher.get_json_data("user.gettoptracks")
+    data_albums = fetcher.get_json_data("user.gettopalbums")
     data_user = fetcher.get_user_info()
-    return data_artists, data_user
+    return data_artists, data_user, data_tracks, data_albums
+
+def get_top_track(data_track):
+    for track in data_track['toptracks']['track'][:1]:
+        top_track = track['name']
+        print(top_track)
+    return top_track
+
+def get_top_album(data_album):
+    for album in data_album['topalbums']['album'][:1]:
+        top_album = album['name']
+        print(top_album)
+    return top_album
 
 def process_data(fetcher, data_artists):
-    top_artists_and_similars = get_top_artists_and_similars(fetcher, data_artists)
-    top_artists_and_genres = get_top_artists_and_genres(fetcher, data_artists)
-    genre_frequency = count_genre_frequency(top_artists_and_genres)
-    sorted_genres = sorted(genre_frequency.items(), key=lambda x: x[1], reverse=True)
-    return sorted_genres, top_artists_and_similars
+    top_artists_and_similars= get_top_artists_and_similars(fetcher, data_artists)
+    return top_artists_and_similars
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         user, period = get_user_input()
         fetcher = DataFetcher(user, period)
-        data_artists, data_user= get_data(fetcher)
-        sorted_genres, top_artists_and_similars = process_data(fetcher, data_artists)
-        return render_template('index.html', sorted_genres=sorted_genres, top_artists_and_similars=top_artists_and_similars, data_user=data_user)
+        data_artists, data_user, data_tracks, data_albums = get_data(fetcher)
+        top_artists_and_similars = process_data(fetcher, data_artists)
+        top_track = get_top_track(data_tracks)
+        top_album = get_top_album(data_albums)
+        plotter = Plotter()
+        data_pie_top_track = plotter.plot_data_pie(data_tracks, "track", user, period)
+        data_pie_top_album = plotter.plot_data_pie(data_albums, "album", user, period)
+
+        return render_template('index.html', period=period, top_track=top_track, top_album=top_album, top_artists_and_similars=top_artists_and_similars, data_user=data_user, data_pie_top_album=data_pie_top_album, data_pie_top_track=data_pie_top_track)
     else: 
         user, period = get_user_input()
         fetcher = DataFetcher(user, period)
         data_artists, data_user= get_data(fetcher)
-        sorted_genres, top_artists_and_similars = process_data(fetcher, data_artists)
-        return render_template('index.html', sorted_genres=sorted_genres, top_artists_and_similars=top_artists_and_similars, data_user=data_user)
+        top_artists_and_similars = process_data(fetcher, data_artists)
+        return render_template('index.html', top_artists_and_similars=top_artists_and_similars, data_user=data_user)
         
 
 if __name__ == '__main__':
